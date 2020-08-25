b0c2512e1854ee47f939f77f0fa457222e87de58
Tallaks <as2328@mail.ru>
2020-08-25 23:45:20 +0300

    Добавил в сцену кнопку "выход", нацепил на нее соответсвуеющий скрипт, кнопка выхода представлена спрайтом

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    Button _thisButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _thisButton = GetComponent<Button>();
+        _thisButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+}
