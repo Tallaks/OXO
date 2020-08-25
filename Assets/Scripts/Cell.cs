6710dfadf0de4973a429ffd56fb7615452733db4
Tallaks <as2328@mail.ru>
2020-08-26 01:53:36 +0300

    Реализовал проверку на выигрыш, при наличии выигрыша сделал механизм оповещения победившего игрока.

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     [SerializeField] private GameController _controller;
+    [SerializeField] private int _horizontalLineNumber;
+    [SerializeField] private int _verticalLineNumber;
 
     private SpriteRenderer _renderer;
 
@@ -16,9 +18,10 @@ public class Cell : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (!_renderer.sprite)
+        if (!_renderer.sprite && !_controller.GameIsFinished)
         {
             _renderer.sprite = _controller.CurrentPlayer == 1 ? _controller.X : _controller.O;
+            _controller.SetCellValue(_horizontalLineNumber, _verticalLineNumber);
             _controller.NextPlayer();
         }
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,10 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private Text _currentPlayerLabel;
 
+    private int[,] _checkingTable;
+
+    public bool GameIsFinished;
+
     public Sprite X; // Спрайт крестика
     public Sprite O; // Спрайт  нолика
 
@@ -16,11 +20,58 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         CurrentPlayer = 1;
+        _checkingTable = new int[3, 3] { {0,0,0},{0,0,0},{0,0,0} };
+        GameIsFinished = false;
     }
 
     public void NextPlayer()
     {
-        CurrentPlayer = CurrentPlayer == 1 ? 2 : 1;
-        _currentPlayerLabel.text = CurrentPlayer == 1 ? "Ходит первый игрок" : "Ходит второй игрок";
+        if (!CheckWin())
+        {
+            CurrentPlayer = CurrentPlayer == 1 ? 2 : 1;
+            _currentPlayerLabel.text = CurrentPlayer == 1 ? "Ходит первый игрок" : "Ходит второй игрок";
+        }
+        else
+        {
+            GameIsFinished = true;
+            _currentPlayerLabel.text = CurrentPlayer == 1 ? "Первый игрок выиграл!" : "Второй игрок выиграл!";
+        }
+    }
+
+    public void SetCellValue(int horizontalIndex, int verticalIndex)
+    {
+        _checkingTable[horizontalIndex-1, verticalIndex-1] = CurrentPlayer;
+    }
+
+    private bool CheckWin()
+    {
+        bool currentPlayerWon = false;
+
+        if (_checkingTable[0, 0] == _checkingTable[1, 1] && _checkingTable[0, 0] == _checkingTable[2, 2] && _checkingTable[0, 0] == CurrentPlayer)
+        {
+            currentPlayerWon = true;
+        }
+
+        if (_checkingTable[2, 0] == _checkingTable[1, 1] && _checkingTable[2, 0] == _checkingTable[0, 2] && _checkingTable[2, 0] == CurrentPlayer)
+        {
+            currentPlayerWon = true;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (_checkingTable[i, 0] == _checkingTable[i, 1] && _checkingTable[i, 0] == _checkingTable[i, 2] && _checkingTable[i, 0] == CurrentPlayer)
+            {
+                currentPlayerWon = true;
+                break;
+            }
+
+            if (_checkingTable[0, i] == _checkingTable[1, i] && _checkingTable[0, i] == _checkingTable[2, i] && _checkingTable[0, i] == CurrentPlayer)
+            {
+                currentPlayerWon = true;
+                break;
+            }
+        }
+
+        return currentPlayerWon;
     }
 }
