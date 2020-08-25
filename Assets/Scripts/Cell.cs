e2a34bb8402137f72a54507ce62052028feb5611
Tallaks <as2328@mail.ru>
2020-08-26 00:59:16 +0300

    Сделал контролллер игры, где проверяется номер игрока и где хранятся спраты для крестика и нолика. Кроме этого добавлен скрипт для ячейки, при нажатии на которую реализуется переход к следующему игроку и рисованию на ячейки символа текущего игрока ( если в данной ячейке не было ничего нарисовано до этого)

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private GameController _controller;
+
+    private SpriteRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (!_renderer.sprite)
+        {
+            _renderer.sprite = _controller.CurrentPlayer == 1 ? _controller.X : _controller.O;
+            _controller.NextPlayer();
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Sprite X; // Спрайт крестика
+    public Sprite O; // Спрайт  нолика
+
+    public int CurrentPlayer; // Номер текущего игрока
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CurrentPlayer = 1;
+    }
+
+    public void NextPlayer()
+    {
+        CurrentPlayer = CurrentPlayer == 1 ? 0 : 1;
+    }
+}
